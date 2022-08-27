f285394b9f8b7948652ce9c961535300aa2d7ab0
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2022-08-27 17:46:33 +0200

    Create cilium_install.sh

---
+++ cilium_install.sh
@@ -0,0 +1,39 @@
+```
+#!/bin/bash
+CILIUM_CLI_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/cilium-cli/master/stable.txt)
+CLI_ARCH=amd64
+if [ "$(uname -m)" = "aarch64" ]; then CLI_ARCH=arm64; fi
+curl -L --fail --remote-name-all https://github.com/cilium/cilium-cli/releases/download/${CILIUM_CLI_VERSION}/cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
+sha256sum --check cilium-linux-${CLI_ARCH}.tar.gz.sha256sum
+sudo tar xzvfC cilium-linux-${CLI_ARCH}.tar.gz /usr/local/bin
+rm cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
+
+#cilium install
+
+cilium install \
+--helm-set ingressController.enabled=true \
+--helm-set securityContext.privileged=true \
+--kube-proxy-replacement=strict \
+--version 1.12.1
+
+#cilium connectivity test
+
+# Enabling Hubble
+cilium hubble enable --ui
+cilium status
+
+# Installing Hubble
+export HUBBLE_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/hubble/master/stable.txt)
+HUBBLE_ARCH=amd64
+if [ "$(uname -m)" = "aarch64" ]; then HUBBLE_ARCH=arm64; fi
+curl -L --fail --remote-name-all https://github.com/cilium/hubble/releases/download/$HUBBLE_VERSION/hubble-linux-${HUBBLE_ARCH}.tar.gz{,.sha256sum}
+sha256sum --check hubble-linux-${HUBBLE_ARCH}.tar.gz.sha256sum
+sudo tar xzvfC hubble-linux-${HUBBLE_ARCH}.tar.gz /usr/local/bin
+rm hubble-linux-${HUBBLE_ARCH}.tar.gz{,.sha256sum}
+
+
+sleep 5 
+cilium hubble port-forward&
+hubble status
+kubectl port-forward -n kube-system svc/hubble-ui --address 0.0.0.0 --address :: 12000:80 > /dev/null &
+```
